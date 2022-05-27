735be240d7f75ca1be1c0fc10116d423902353a5
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-05-27 19:23:07 -0300

    Indented lines inside showSlides function

--- js/anime-info-script.js
+++ js/anime-info-script.js
@@ -94,14 +94,14 @@ function changeSlide(n) {
 }
 
 async function showSlides(n) {
-  let slides = await document.getElementsByClassName("slides")
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  console.log(slides[0])
-  slides[slideIndex-1].style.display = "block";
+    let slides = await document.getElementsByClassName("slides")
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    console.log(slides[0])
+    slides[slideIndex-1].style.display = "block";
 }
 
 async function startAnimeInfoPage() {
