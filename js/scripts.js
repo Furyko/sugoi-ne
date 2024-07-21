2fa46271aeb389b003bc8793ce7a35fddc8f8ca1
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2024-07-21 02:55:37 -0300

    Add dark mode functionality
    Minor refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 const switchModeBtn = document.getElementById('switch-mode-button')
 switchModeBtn.addEventListener('click', (e) => {
-    document.documentElement.classList.toggle('dark-mode');
+    toggleDarkMode()
 })
+
+function toggleDarkMode() {
+    document.documentElement.classList.toggle('dark-mode');
+    if (document.documentElement.classList.contains('dark-mode')) localStorage.setItem("dark-mode", true)
+    else localStorage.setItem("dark-mode", false)
+}
+
+function startPage() {
+    const darkMode = localStorage.getItem('dark-mode')
+    if (darkMode && darkMode == 'true') toggleDarkMode()
+}
+
+setTimeout(function() { startPage() }, 1);
